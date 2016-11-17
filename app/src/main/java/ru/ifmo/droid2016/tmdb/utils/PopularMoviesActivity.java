5b0860b6c49c186fb2eb932a5e9b31727c535a97
sofatitovass <sof_titova@rambler.ru>
2016-11-17 23:35:12 +0300

    first version

---
+++ app/src/main/java/ru/ifmo/droid2016/tmdb/utils/PopularMoviesActivity.java
@@ -0,0 +1,123 @@
+package ru.ifmo.droid2016.tmdb.utils;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ru.ifmo.droid2016.tmdb.R;
+import ru.ifmo.droid2016.tmdb.loader.LoadResult;
+import ru.ifmo.droid2016.tmdb.loader.ResultType;
+import ru.ifmo.droid2016.tmdb.loader.TmbLoader;
+import ru.ifmo.droid2016.tmdb.model.Movie;
+import ru.ifmo.droid2016.tmdb.utils.RecylcerDividersDecorator;
+
+/**
+ * Экран, отображающий список популярных фильмов из The Movie DB.
+ */
+public class PopularMoviesActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<LoadResult<List<Movie>>> {
+
+
+    public static final String EXTRA_LATITUDE = "lat";
+    public static final String EXTRA_LONGITUDE = "lng";
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressView;
+    private TextView errorTextView;
+
+    @Nullable
+    private MoviesRecyclerAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_movies);
+        progressView = (ProgressBar) findViewById(R.id.progress);
+        errorTextView = (TextView) findViewById(R.id.error_text);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(
+                new RecylcerDividersDecorator(getResources().getColor(R.color.gray_a)));
+
+        progressView.setVisibility(View.VISIBLE);
+        errorTextView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+
+
+        final Bundle loaderArgs = getIntent().getExtras();
+        getSupportLoaderManager().initLoader(0, loaderArgs, this);
+    }
+
+    @Override
+    public Loader<LoadResult<List<Movie>>> onCreateLoader(int id, Bundle args) {
+        return new TmbLoader(this);
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<LoadResult<List<Movie>>> loader,
+                               LoadResult<List<Movie>> result) {
+        if (result.resultType == ResultType.OK) {
+            if (result.data != null && !result.data.isEmpty()) {
+                displayNonEmptyData(result.data);
+            } else {
+                displayEmptyData();
+            }
+        } else {
+            displayError(result.resultType);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<LoadResult<List<Movie>>> loader) {
+        displayEmptyData();
+    }
+
+
+    private void displayEmptyData() {
+        progressView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.VISIBLE);
+        errorTextView.setText("Не удалось найти фильмы");
+    }
+
+    private void displayNonEmptyData(List<Movie> movies) {
+        if (adapter == null) {
+            adapter = new MoviesRecyclerAdapter(this);
+            recyclerView.setAdapter(adapter);
+        }
+        adapter.setMovies(movies);
+        progressView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void displayError(ResultType resultType) {
+        progressView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.VISIBLE);
+        final int messageResId;
+        if (resultType == ResultType.NO_INTERNET) {
+            messageResId = R.string.no_inernet;
+        } else {
+            messageResId = R.string.error;
+        }
+        errorTextView.setText(messageResId);
+    }
+
+    private static final String TAG = "Movies";
+
+}
+
+
+
