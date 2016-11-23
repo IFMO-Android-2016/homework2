94c716e297195fa58e843602ed32192c0f218feb
Serafimova Ksenia <serafimova@rain.ifmo.ru>
2016-11-23 23:18:54 +0300

    Update TmdbApi.java

--- app/src/main/java/ru/ifmo/droid2016/tmdb/api/TmdbApi.java
+++ app/src/main/java/ru/ifmo/droid2016/tmdb/api/TmdbApi.java
@@ -13,8 +13,7 @@ import java.net.URL;
  */
 public final class TmdbApi {
 
-    // TODO: Зарегистрироваться на https://www.themoviedb.org и получить свой собственный ключ
-    //private static final String API_KEY = "ee1c42c80c58d28bc54efc844b63d114";
+    private static final String API_KEY = "51651539ef38c1dea197e0331fcd7ea7";
 
     private static final Uri BASE_URI = Uri.parse("https://api.themoviedb.org/3");
 
@@ -29,7 +28,12 @@ public final class TmdbApi {
      * @param lang язык пользователя
      */
     public static HttpURLConnection getPopularMoviesRequest(String lang) throws IOException {
-        // TODO
-        return (HttpURLConnection) new URL(BASE_URI.toString()).openConnection();
+        Uri uri = BASE_URI.buildUpon()
+                .appendPath("movie")
+                .appendPath("popular")
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("language", lang)
+                .build();
+        return (HttpURLConnection) new URL(uri.toString()).openConnection();
     }
 }
