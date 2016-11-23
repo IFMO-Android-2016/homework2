3e6abfd17127b9a4b56df0271f35de77ffb252cb
kant97 <kirant97@mail.ru>
2016-11-23 23:05:30 +0300

    Cool

--- app/src/main/java/ru/ifmo/droid2016/tmdb/PopularMoviesActivity.java
+++ app/src/main/java/ru/ifmo/droid2016/tmdb/PopularMoviesActivity.java
@@ -1,17 +1,114 @@
 package ru.ifmo.droid2016.tmdb;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ru.ifmo.droid2016.tmdb.loader.LoadResult;
+import ru.ifmo.droid2016.tmdb.loader.MoviesLoader;
+import ru.ifmo.droid2016.tmdb.loader.ResultType;
+import ru.ifmo.droid2016.tmdb.model.Movie;
+import ru.ifmo.droid2016.tmdb.utils.RecylcerDividersDecorator;
 
 /**
  * Экран, отображающий список популярных фильмов из The Movie DB.
  */
-public class PopularMoviesActivity extends AppCompatActivity {
+public class PopularMoviesActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<LoadResult<List<Movie>>> {
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressView;
+    private TextView errorTextView;
+
+    @Nullable
+    private MoviesAdapter moviesAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popular_movies);
+        progressView = (ProgressBar) findViewById(R.id.progress);
+        errorTextView = (TextView) findViewById(R.id.error_text);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(
+                new RecylcerDividersDecorator(getResources().getColor(R.color.gray_a)));
+
+        progressView.setVisibility(View.VISIBLE);
+        errorTextView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+
+        // Pass all extra params directly to loader
+        final Bundle loaderArgs = getIntent().getExtras();
+        getSupportLoaderManager().initLoader(0, loaderArgs, this);
     }
 
+    @Override
+    public Loader<LoadResult<List<Movie>>> onCreateLoader(int id, Bundle args) {
+        return new MoviesLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<LoadResult<List<Movie>>> loader,
+                               LoadResult<List<Movie>> result) {
+        if (loader == null || result == null) {
+            displayError(ResultType.NO_INTERNET);
+            return;
+        }
+        if (result.resultType == ResultType.OK) {
+            if (result.data != null && !result.data.isEmpty()) {
+                displayNonEmptyData(result.data);
+            } else {
+                displayEmptyData();
+            }
+        } else {
+            displayError(result.resultType);
+        }
+    }
+
+    private void displayEmptyData() {
+        progressView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.VISIBLE);
+        errorTextView.setText("Филмы не найдены");
+    }
+
+    private void displayNonEmptyData(List<Movie> movies) {
+        if (moviesAdapter == null) {
+            moviesAdapter = new MoviesAdapter(this);
+            recyclerView.setAdapter(moviesAdapter);
+        }
+        moviesAdapter.setMovies(movies);
+        progressView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void displayError(ResultType resultType) {
+        progressView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.VISIBLE);
+        final int messageResId;
+        if (resultType == ResultType.NO_INTERNET) {
+            messageResId = R.string.no_inernet;
+        } else {
+            messageResId = R.string.error;
+        }
+        errorTextView.setText(messageResId);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<LoadResult<List<Movie>>> loader) {
+        displayEmptyData();
+    }
 }
