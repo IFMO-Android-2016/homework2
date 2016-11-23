8f98b5467ae53d6df08212d031d4a203dd648857
Impy <impy.bian@gmail.com>
2016-11-23 21:39:52 +0300

    First commit

--- app/src/main/java/ru/ifmo/droid2016/tmdb/PopularMoviesActivity.java
+++ app/src/main/java/ru/ifmo/droid2016/tmdb/PopularMoviesActivity.java
@@ -1,17 +1,105 @@
 package ru.ifmo.droid2016.tmdb;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ru.ifmo.droid2016.tmdb.loader.LoadResult;
+import ru.ifmo.droid2016.tmdb.loader.MovieLoader;
+import ru.ifmo.droid2016.tmdb.loader.ResultType;
+import ru.ifmo.droid2016.tmdb.model.Movie;
+import ru.ifmo.droid2016.tmdb.utils.RecylcerDividersDecorator;
 
 /**
  * Экран, отображающий список популярных фильмов из The Movie DB.
  */
-public class PopularMoviesActivity extends AppCompatActivity {
+public class PopularMoviesActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<LoadResult<List<Movie>>> {
+    private RecyclerView recyclerView;
+    private ProgressBar progressView;
+    private TextView errorTextView;
+
+
+    @Nullable
+    private WebcamsRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popular_movies);
+
+
+        progressView = (ProgressBar) findViewById(R.id.progressBar);
+        errorTextView = (TextView) findViewById(R.id.error_massage);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(
+                new RecylcerDividersDecorator(getResources().getColor(R.color.colorPrimaryDark)));
+
+        progressView.setVisibility(View.VISIBLE);
+        errorTextView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+
+        // Pass all extra params directly to loader
+        final Bundle loaderArgs = getIntent().getExtras();
+        getSupportLoaderManager().initLoader(0, loaderArgs, this);
     }
 
+
+    @Override
+    public Loader<LoadResult<List<Movie>>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<LoadResult<List<Movie>>> loader, LoadResult<List<Movie>> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<LoadResult<List<Movie>>> loader) {
+
+    }
+
+    private void displayEmptyData() {
+        progressView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void displayNonEmptyData(List<Movie> webcams) {
+        if (adapter == null) {
+            adapter = new WebcamsRecyclerAdapter(this);
+            recyclerView.setAdapter(adapter);
+        }
+        adapter.setWebcams(webcams);
+        progressView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void displayError(ResultType resultType) {
+        progressView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.VISIBLE);
+        final int messageResId;
+        if (resultType == ResultType.NO_INTERNET) {
+            messageResId = R.string.no_inernet;
+        } else {
+            messageResId = R.string.error;
+        }
+        errorTextView.setText(messageResId);
+    }
+
+    private static final String TAG = "Webcams";
 }
--- app/src/main/java/ru/ifmo/droid2016/tmdb/api/TmdbApi.java
+++ app/src/main/java/ru/ifmo/droid2016/tmdb/api/TmdbApi.java
@@ -14,9 +14,9 @@ import java.net.URL;
 public final class TmdbApi {
 
     // TODO: Зарегистрироваться на https://www.themoviedb.org и получить свой собственный ключ
-    //private static final String API_KEY = "ee1c42c80c58d28bc54efc844b63d114";
+    private static final String API_KEY = "62009868f1b751608eed22340d59da62";
 
-    private static final Uri BASE_URI = Uri.parse("https://api.themoviedb.org/3");
+    private static final String BASE_URL = "https://api.themoviedb.org/3";
 
 
     private TmdbApi() {}
@@ -28,8 +28,15 @@ public final class TmdbApi {
      *
      * @param lang язык пользователя
      */
-    public static HttpURLConnection getPopularMoviesRequest(String lang) throws IOException {
-        // TODO
-        return (HttpURLConnection) new URL(BASE_URI.toString()).openConnection();
+    public static HttpURLConnection getPopularMoviesRequest(String lang, int page) throws IOException {
+
+        Uri uri = Uri.parse(BASE_URL).buildUpon()
+                .appendPath("movie")
+                .appendPath("popular")
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("language", lang)
+                .appendQueryParameter("page", "1")
+                .build();
+        return (HttpURLConnection) new URL(uri.toString()).openConnection();
     }
 }
