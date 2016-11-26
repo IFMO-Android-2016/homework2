ef03013512b0d8944c8075ec58b7028165583a68
Ivan Trofimov <richard1997dog@gmail.com>
2016-11-26 23:01:03 +0300

    fix wrong merge

--- script/app-config.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('BlankApp', ['ngMaterial'])
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('red')
-    .accentPalette('blue')
-});
